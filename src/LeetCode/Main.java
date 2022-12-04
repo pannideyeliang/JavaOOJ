4249f110e72b4fee40f60a30488f2729fe0194c9
chenjiale <2601823097@qq.com>
2022-12-04 17:34:36 +0800

    leetcode 2 两数相加

--- src/LeetCode/Main.java
+++ src/LeetCode/Main.java
@@ -1,14 +1,12 @@
 package LeetCode;
 
 import java.util.HashMap;
-import java.util.function.BinaryOperator;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(14/10);
     }
 }
-
 
 //1. 两数之和
 class Solution {
@@ -31,8 +29,80 @@ class Solution {
             if (hashtable.containsKey(target - nums[i])) {
                 return new int[]{hashtable.get(target - nums[i]), i};
             }
-            hashtable.put(nums[i],i);
+            hashtable.put(nums[i], i);
         }
         return new int[]{0};
     }
+
+    //自己能看懂的版本
+    public int[] twoSumV3(int[] nums, int target) {
+        HashMap<Integer, Integer> hashtable = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            hashtable.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (hashtable.containsKey(target - nums[i]) && i != hashtable.get(target - nums[i])) {
+//                上面的判断是为了防止自己和自己相加得到target
+                return new int[]{hashtable.get(target - nums[i]), i};
+            }
+        }
+        return new int[]{0};
+    }
+
+    //    两数相加
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//          去除等于0的情况
+        if (l1.val == 0) return l2;
+        if (l2.val == 0) return l1;
+
+        Integer count = 0;      //存储进位
+        Integer num = 0;        //存储余数
+
+        while (l1.next != null l2.next != null){
+            count = l1.val + l2.val
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
